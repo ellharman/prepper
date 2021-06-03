33fb252f45634c8ea83536f138bfbfeaee3cc2cc
Ell Harman <78617707+ellharman@users.noreply.github.com>
2021-06-03 12:24:46 +0100

    Add screen width detection + redirect

---
+++ src/views/Init.vue
@@ -0,0 +1,25 @@
+<template>
+  <div />
+</template>
+<script>
+export default {
+  name: 'Init',
+  methods: {
+    isMobile() {
+      if (window.screen.width <= 760) {
+        return true;
+      }
+      return false;
+    },
+  },
+  created() {
+    if (this.isMobile()) {
+      this.$router.push('/MainMobile');
+    } else {
+      this.$router.push('/Main');
+    }
+  },
+};
+</script>
+<style scoped>
+</style>

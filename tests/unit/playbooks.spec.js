53386bad6454468e50e41358acf9c6ec7686c36b
Ell Harman <78617707+ellharman@users.noreply.github.com>
2021-06-04 17:44:22 +0100

    Add testing for all playbook files

--- tests/unit/playbooks.spec.js
+++ tests/unit/playbooks.spec.js
@@ -1,16 +1,28 @@
 import glob from 'glob-promise';
+import Ajv from 'ajv';
+import schema from '../../src/assets/schema.json';
 
 // I love the globber and the globber loves me
+// Gets the paths of all playbook JSONs in the playbooks folder
+const playbooks = glob.sync('src/assets/playbooks/**/*.json');
 
-let playbooks;
+const ajv = new Ajv();
 
-beforeAll(() => glob('src/assets/playbooks/**/*.json').then((contents) => {
-  playbooks = contents;
-}));
-
-describe('Playbooks', () => {
+describe('Playbook loading', () => {
   it('should be testing against an array of paths to playbook JSONs', () => {
-    console.log(playbooks);
     expect(playbooks).toBeInstanceOf(Array);
   });
+  // This test validates each of the playbooks at the paths in src/assets/playbooks against
+  // the schema at src/assets/
+  test.each(playbooks)(
+    'validate JSON at %s',
+    (path) => {
+      // eslint-disable-next-line global-require, import/no-dynamic-require
+      const jsonToValidate = require(`../../${path}`);
+      const validate = ajv.compile(schema);
+      const valid = validate(jsonToValidate);
+      if (!valid) console.log(`Playbook JSON failed to validate with ${JSON.stringify(validate.errors[0].message)} at JSON path ${JSON.stringify(validate.errors[0].instancePath)}`);
+      expect(valid).toBe(true);
+    },
+  );
 });

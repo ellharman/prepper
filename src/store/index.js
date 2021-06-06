3f960ba53fd60b8154bb55101c936e0b4a477d65
Ell Harman <78617707+ellharman@users.noreply.github.com>
2021-06-07 00:44:28 +0100

    Add playbook state

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,13 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    selectedPlaybook: '',
   },
   mutations: {
+    updateSelectedPlaybook(state, input) {
+      state.selectedPlaybook = input;
+      console.log(`Updated current playbook to ${state.selectedPlaybook}`);
+    },
   },
   actions: {
   },

41de29d24a74f7520d47fbe4493d2ecb5bb46edf
Ell Harman <78617707+ellharman@users.noreply.github.com>
2021-06-10 17:48:32 +0100

    Styling

--- src/components/CharacterInfo.vue
+++ src/components/CharacterInfo.vue
@@ -4,6 +4,7 @@
       <b-row align-h="center" class="header">
         {{ playbook.className }}
       </b-row>
+            <hr class="rule" id="initial-rule" />
       <b-row align-v="center">
         <b-col class="body">
           <div class="text-center">
@@ -41,46 +42,89 @@
           />
         </b-col>
       </b-row>
-      <b-row> <b-col class="body">Alignments</b-col></b-row>
-      <b-row
-        ><b-col class="body">
-          <b-list-group>
-            <checkbox-info
-              class="checkbox-info"
-              v-for="(alignment, index) in alignments"
-              :playbookPath="'alignments'"
-              :index="index"
-              :key="alignments[index][2]"
-            ></checkbox-info></b-list-group></b-col
-      ></b-row>
-      <b-row> <b-col class="body">Backgrounds</b-col></b-row>
-      <b-row
-        ><b-col class="body">
-          <b-list-group>
-            <checkbox-info
-              class="checkbox-info"
-              v-for="(background, index) in backgrounds"
-              :playbookPath="'backgrounds'"
-              :index="index"
-              :key="backgrounds[index][2]"
-            ></checkbox-info></b-list-group></b-col
-      ></b-row>
+      <hr class="rule" />
+      <div class="subcontainer">
+        <b-row> <b-col class="body">Alignments</b-col></b-row>
+        <b-row
+          ><b-col class="body">
+            <b-list-group>
+              <checkbox-info
+                class="checkbox-info"
+                v-for="(alignment, index) in alignments"
+                :playbookPath="'alignments'"
+                :index="index"
+                :key="alignments[index][2]"
+              ></checkbox-info></b-list-group></b-col
+        ></b-row>
+      </div>
+      <hr class="rule" />
+      <div class="subcontainer">
+        <b-row> <b-col class="body">Backgrounds</b-col></b-row>
+        <b-row
+          ><b-col class="body">
+            <b-list-group>
+              <checkbox-info
+                class="checkbox-info"
+                v-for="(background, index) in backgrounds"
+                :playbookPath="'backgrounds'"
+                :index="index"
+                :key="backgrounds[index][2]"
+              ></checkbox-info></b-list-group></b-col
+        ></b-row>
+      </div>
+            <hr class="rule" />
     </b-col>
   </b-container>
 </template>
 
 <style lang="scss" scoped>
+
+.rule {
+  border-color: #1cb84393;
+}
+
+#initial-rule {
+  margin-top: 0px;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 0.5em;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #1cb843;
+}
+
 .header {
   padding: 0.2em;
-  font-size: 2em;
+  font-size: 2.5em;
+}
+
+.subcontainer {
+  margin-top: 1em;
+  margin-bottom: 1em;
+  padding-left: 2em;
+  padding-right: 2em;
 }
 
 .body {
   font-size: 1.2em;
   padding-left: 0px;
   padding-right: 0px;
-  padding-top: 0.4em;
-  padding-bottom: 0.4em;
+  padding-top: 0.2em;
+  padding-bottom: 0.2em;
 }
 
 input,
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="main">
-    Main
-    <b-container class="grid-container" fluid>
+    <b-container class="grid-container" id="main-container" fluid>
       <b-row>
         <b-col>
           <character-info />
         </b-col>
-        <b-col>
-        </b-col>
-        <b-col>
-          moves
-        </b-col>
+        <b-col> </b-col>
+        <b-col> moves </b-col>
       </b-row>
     </b-container>
     <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button>
@@ -18,13 +14,11 @@
 </template>
 
 <style lang="scss" scoped>
-
 .main {
   background-color: #060e1d;
   color: #f5c884;
   height: 100vh;
 }
-
 .btn {
   border: white;
   font-size: 1em;
@@ -32,10 +26,31 @@
   color: inherit;
 }
 
-.col {
-  border: solid;
+/* width */
+::-webkit-scrollbar {
+  width: 0.5em;
 }
 
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #1cb843;
+}
+
+.col {
+  // border: solid;
+  height: 100vh;
+  overflow-y: scroll;
+}
 </style>
 
 <script>
